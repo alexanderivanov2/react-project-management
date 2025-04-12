770ab1d202e9470b8648a3bb3d5869d674a16d8b
alexanderivanov2 <alexanderivanov2277@gmail.com>
2025-04-12 23:26:50 +0300

    add styles for component in main module.scss

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import styles from './Main.module.scss'
+import NoProjectSelected from './NoProjectSelected'
+import AddProject from './AddProject'
 
 function Main() {
   return (
-    <div className={styles.mainContainer}>Main</div>
+    <div className={styles.mainContainer}>
+        {/* <NoProjectSelected /> */}
+        <AddProject />
+    </div>
   )
 }
 
