78ebe32d1acd0da0f1a45dc31988d32388b41ada
alexanderivanov2 <alexanderivanov2277@gmail.com>
2025-04-12 23:26:01 +0300

    Implement base of add project

---
+++ src/components/Main/AddProject.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import styles from './Main.module.scss'
+function AddProject() {
+    return (
+        <div className={`${styles.addProject} ${styles.mainWrapper}`}>
+            <form action="submit">
+                <div className={styles.btnsWrapper}>
+                    <button className={`${styles.mainBtn}`}>Cancel</button>
+                    <button className={`${styles.mainBtn} ${styles.mainPrimaryBtn}`}>Save</button>
+                </div>
+                <fieldset>
+                    <label htmlFor="projectTitle">Title</label>
+                    <input type="text" name="projectTitle" id='projectTitle'/>
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="projectDescription">Description</label>
+                    <textarea name="projectDescription" id="projectDescription"></textarea>
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="projectDueDate">Due Date</label>
+                    <input type="date" name="projectDueDate" id="projectDueDate" />
+                </fieldset>
+            </form>
+        </div>
+    )
+}
+
+export default AddProject
