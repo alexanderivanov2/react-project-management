0d122e9b6f0d506fac795c5e5b0407da053858b0
alexanderivanov2 <alexanderivanov2277@gmail.com>
2025-04-13 12:25:33 +0300

    add change page logic

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import styles from './Sidebar.module.scss'
 
-function Sidebar() {
+function Sidebar({changePage}) {
   return (
     <div className={styles.sidebarContainer}>
         <div className={styles.sidebarContent}>
             <h2 className={styles.sidebarTitle}>Your Projects</h2>
-            <button className={styles.sidebarAddBtn}>+ Add Project</button>
+            <button className={styles.sidebarAddBtn} onClick={() => changePage('addProject')}>+ Add Project</button>
         </div>
     </div>
   )
