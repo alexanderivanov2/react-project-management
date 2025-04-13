b94f5b0b9cd206db1645a5ec87703b1354a3f906
alexanderivanov2 <alexanderivanov2277@gmail.com>
2025-04-13 14:01:32 +0300

    implement add project funtionality

--- src/components/Main/AddProject.jsx
+++ src/components/Main/AddProject.jsx
@@ -1,29 +1,49 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import styles from './Main.module.scss'
-function AddProject({changePage}) {
+function AddProject({changePage, updateProjects}) {
+    const projectTitleRef = useRef();
+    const projectDescriptionRef = useRef();
+    const projectDueDateRef = useRef();
 
     const handleCancel = (e) => {
         e.preventDefault();
         changePage('noProject')
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const id = Math.ceil((Math.random() * 100) * 99);
+        const name = projectTitleRef.current.value ? projectTitleRef.current.value : 'No Name';
+        const dueDate = projectDueDateRef.current.value ? projectDueDateRef.current.value : new Date().toLocaleDateString();
+        const newProject = {
+            id,
+            name,
+            description: projectDescriptionRef.current.value,
+            dueDate,
+        }
+
+        updateProjects('add', newProject);
+        changePage('project');
     }
     return (
         <div className={`${styles.addProject} ${styles.mainWrapper}`}>
             <form action="submit">
                 <div className={styles.btnsWrapper}>
                     <button className={`${styles.mainBtn}`} onClick={handleCancel}>Cancel</button>
-                    <button className={`${styles.mainBtn} ${styles.mainPrimaryBtn}`}>Save</button>
+                    <button className={`${styles.mainBtn} ${styles.mainPrimaryBtn}`} onClick={handleSubmit}>Save</button>
                 </div>
                 <fieldset>
                     <label htmlFor="projectTitle">Title</label>
-                    <input type="text" name="projectTitle" id='projectTitle'/>
+                    <input type="text" name="projectTitle" id='projectTitle' ref={projectTitleRef}/>
                 </fieldset>
                 <fieldset>
                     <label htmlFor="projectDescription">Description</label>
-                    <textarea name="projectDescription" id="projectDescription"></textarea>
+                    <textarea name="projectDescription" id="projectDescription" ref={projectDescriptionRef}></textarea>
                 </fieldset>
                 <fieldset>
                     <label htmlFor="projectDueDate">Due Date</label>
-                    <input type="date" name="projectDueDate" id="projectDueDate" />
+                    <input type="date" name="projectDueDate" id="projectDueDate"  ref={projectDueDateRef}/>
                 </fieldset>
             </form>
         </div>
