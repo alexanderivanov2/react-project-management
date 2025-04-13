478e13e1ff78fd657affb9d6fc06c47cc5ab68c0
alexanderivanov2 <alexanderivanov2277@gmail.com>
2025-04-13 12:25:12 +0300

    lift change page logic

--- src/components/Management.jsx
+++ src/components/Management.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import { useState } from 'react'
 import Sidebar from './Sidebar/Sidebar'
 import Main from './Main/Main'
 import styles from './Management.module.scss'
 
 function Management() {
+  const [currentPage, setCurrentPage] = useState('noProject');
+
+  const handleChangePage = (pageName) => {
+    setCurrentPage(pageName);
+  }
+
+
   return (
-    <div className={styles.managementContainer }>
-        <Sidebar />
-        <Main />
+    <div className={styles.managementContainer}>
+      <Sidebar changePage={handleChangePage} />
+      <Main changePage={handleChangePage} page={currentPage} />
     </div>
   )
 }
